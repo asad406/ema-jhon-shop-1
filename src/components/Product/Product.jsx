3f2e37a34ec971c21911643bf4d1fc96c6f538ab
asad406 <asadl76@gmail.com>
2023-03-27 16:03:09 +0600

    data transfar child to parent by state-lifting

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -2,19 +2,26 @@ import React from 'react';
 import './Product.css';
 
 const Product = (props) => {
-  console.log(props.product);
-  const {id, name, price, ratings, seller, img, quantity } = props.product;
+  const { id, name, price, ratings, seller, img, quantity } = props.product;
+  const handleAddToCart = props.handleAddToCart;
   return (
     <div className="product">
-      <img src={img} alt=""/>
-      <div className='product-info'>
+      <img
+        src={img}
+        alt=""
+      />
+      <div className="product-info">
         <h6 className="product-name">{name}</h6>
         <p>Price: {price}</p>
         <p>Manufacturer: {seller}</p>
         <p>Rating: {ratings}</p>
-        
       </div>
-      <button className='btn-cart'>Add to Cart</button>
+      <button
+        onClick={() => handleAddToCart(props.product)}
+        className="btn-cart"
+      >
+        Add to Cart
+      </button>
     </div>
   );
 };
